a4cb7e40bfd3fd3cae8f23cf811ea3cdcae918de
Canerelpacino <canersmedia@gmail.com>
2021-12-24 13:24:39 +0100

    design

--- src/views/c4w1.vue
+++ src/views/c4w1.vue
@@ -1,5 +1,8 @@
 <template>
-
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <body>
+  <img src="../assets/b2.png" width="350" height="350">
+  </body>
 </template>
 
 <script>
