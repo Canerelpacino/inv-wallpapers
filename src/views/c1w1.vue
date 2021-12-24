6d273d78f6ef24d229ab212ddb2554fd71912f17
Canerelpacino <canersmedia@gmail.com>
2021-12-24 16:37:48 +0100

    design

--- src/views/c1w1.vue
+++ src/views/c1w1.vue
@@ -1,6 +1,11 @@
 <template>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <body>
+  <div>&nbsp;</div>
+  <div>&nbsp;</div>
+  <div>&nbsp;</div>
+  <div>&nbsp;</div>
+  <div>&nbsp;</div>
   <img src="../assets/c1w1.png" width="230.712891" height="500" class="wallpaper">
   </body>
 </template>
